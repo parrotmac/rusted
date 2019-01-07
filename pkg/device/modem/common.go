37fde3385bdf99d9e9249fc2942fcc1dd4b85e18
Isaac Parker <parrotmac@gmail.com>
2019-01-07 13:49:02 -0800

    Create Modem common interface & sms structures

---
+++ pkg/device/modem/common.go
@@ -0,0 +1,36 @@
+package modem
+
+import (
+	"github.com/parrotmac/rusted/pkg/central"
+)
+
+type CellModem interface {
+}
+
+type SMSMessage struct {
+	outgoing           bool
+	destinationAddress string
+	sendingAddress     string
+	messageBody        []byte //
+	read               bool   // Modem/SIM tracks read/unread
+	messageId          int    // Modem/SIM's message identifier (from e.g. AT+CMGL/AT+CMGR)
+}
+
+func CreateOutgoingSMS(destination string, body []byte) SMSMessage {
+	return SMSMessage{
+		outgoing:           true,
+		destinationAddress: destination,
+		messageBody:        body,
+
+		// TODO: Should we keep the same structure for unwritten messages?
+		// Some of these fields might make sense once applied to a given modem
+		sendingAddress: "",
+		read:           false,
+		messageId:      -1,
+	}
+}
+
+type SmsService interface {
+	SendMessage(ctx *central.Context, message SMSMessage) error
+	GetAllMessages(ctx *central.Context) ([]SMSMessage, error)
+}
